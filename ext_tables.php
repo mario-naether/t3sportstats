d22f435a7fcb65a12932319982c6f1b8e5872b78
René Nitzsche <rene@system25.de>
2015-11-25 10:46:33 +0100

    BE-Module deaktiviert

--- ext_tables.php
+++ ext_tables.php
@@ -10,28 +10,29 @@ $TCA['tt_content']['types']['list']['subtypes_excludelist']['tx_t3sportstats']='
 // Das tt_content-Feld pi_flexform einblenden
 $TCA['tt_content']['types']['list']['subtypes_addlist']['tx_t3sportstats']='pi_flexform';
 
-t3lib_extMgm::addPiFlexFormValue('tx_t3sportstats','FILE:EXT:'.$_EXTKEY.'/flexform_main.xml');
-t3lib_extMgm::addPlugin(Array('LLL:EXT:'.$_EXTKEY.'/locallang_db.php:plugin.t3sportstats.label','tx_t3sportstats'));
+tx_rnbase::load('tx_rnbase_util_Extensions');
+tx_rnbase_util_Extensions::addPiFlexFormValue('tx_t3sportstats','FILE:EXT:'.$_EXTKEY.'/flexform_main.xml');
+tx_rnbase_util_Extensions::addPlugin(Array('LLL:EXT:'.$_EXTKEY.'/locallang_db.php:plugin.t3sportstats.label','tx_t3sportstats'));
 
 
-t3lib_extMgm::addStaticFile($_EXTKEY,'static/ts/', 'T3sportstats');
+tx_rnbase_util_Extensions::addStaticFile($_EXTKEY,'static/ts/', 'T3sportstats');
 
 if (TYPO3_MODE=="BE")	{
 	# Add plugin wizard
 	$TBE_MODULES_EXT['xMOD_db_new_content_el']['addElClasses']['tx_t3sportstats_util_wizicon'] = t3lib_extMgm::extPath($_EXTKEY).'util/class.tx_t3sportstats_util_Wizicon.php';
 
 	// Einbindung einer PageTSConfig
-	t3lib_extMgm::addPageTSConfig('<INCLUDE_TYPOSCRIPT: source="FILE:EXT:t3sportstats/mod/pageTSconfig.txt">');
+	tx_rnbase_util_Extensions::addPageTSConfig('<INCLUDE_TYPOSCRIPT: source="FILE:EXT:t3sportstats/mod/pageTSconfig.txt">');
 
 	////////////////////////////////
 	// Submodul anmelden
 	////////////////////////////////
-	t3lib_extMgm::insertModuleFunction(
-		'web_txcfcleagueM1',
-		'tx_t3sportstats_mod_index',
-		t3lib_extMgm::extPath($_EXTKEY).'mod/class.tx_t3sportstats_mod_index.php',
-		'LLL:EXT:t3sportstats/mod/locallang.xml:tx_t3sportstats_module_name'
-	);
+// 	tx_rnbase_util_Extensions::insertModuleFunction(
+// 		'web_txcfcleagueM1',
+// 		'tx_t3sportstats_mod_index',
+// 		tx_rnbase_util_Extensions::extPath($_EXTKEY).'mod/class.tx_t3sportstats_mod_index.php',
+// 		'LLL:EXT:t3sportstats/mod/locallang.xml:tx_t3sportstats_module_name'
+// 	);
 }
 
 
