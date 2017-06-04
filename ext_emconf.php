3fe89f68162cbde60eeca4c0a47f229cf66471a4
Mario Näther <mario-naether@gmx.de>
2017-06-04 10:37:48 +0200

    - raised typo3 version to 8.7

--- ext_emconf.php
+++ ext_emconf.php
@@ -32,9 +32,9 @@ $EM_CONF[$_EXTKEY] = array(
 	'lockType' => '',
 	'constraints' => array(
 		'depends' => array(
-			'typo3' => '4.3.0-6.2.99',
-			'rn_base' => '0.14.0-0.0.0',
-			'cfc_league' => '0.9.1-0.0.0',
+			'typo3' => '4.3.0-8.7.99',
+			'rn_base' => '1.14.0-0.0.0',
+			'cfc_league' => '1.1.1-0.0.0',
 			'cfc_league_fe' => '0.9.0-0.0.0',
 		),
 		'conflicts' => array(
