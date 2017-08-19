5802dba8d1026a87d9b95edc1080dd6518e1a728
René Nitzsche <rene@system25.de>
2017-08-19 21:38:23 +0200

    Code style

--- search/class.tx_t3sportstats_search_Builder.php
+++ search/class.tx_t3sportstats_search_Builder.php
@@ -2,7 +2,7 @@
 /***************************************************************
  *  Copyright notice
  *
- *  (c) 2010-2016 Rene Nitzsche
+ *  (c) 2010-2017 Rene Nitzsche
  *  Contact: rene@system25.de
  *  All rights reserved
  *
@@ -20,73 +20,76 @@
  * License along with this library; if not, write to the Free Software
  * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
  ***************************************************************/
-
 tx_rnbase::load('tx_rnbase_util_SearchBase');
-
 
 /**
  * Mit dem Builder werden haufig auftretende Suchanfragen zusammengebaut
  *
  * @author Rene Nitzsche
  */
-class tx_t3sportstats_search_Builder {
+class tx_t3sportstats_search_Builder
+{
+    /**
+     * Search for player statistics by scope
+     *
+     * @param array $fields
+     * @param string $scope
+     *            Scope Array
+     * @return true
+     */
+    public static function buildPlayerStatsByScope(&$fields, $scope)
+    {
+        $result = false;
+        $result = self::setField($fields, 'PLAYERSTAT.SAISON', OP_IN_INT, $scope['SAISON_UIDS']) || $result;
+        $result = self::setField($fields, 'PLAYERSTAT.AGEGROUP', OP_IN_INT, $scope['GROUP_UIDS']) || $result;
+        $result = self::setField($fields, 'PLAYERSTAT.COMPETITION', OP_IN_INT, $scope['COMP_UIDS']) || $result;
+        $result = self::setField($fields, 'PLAYERSTAT.CLUB', OP_IN_INT, $scope['CLUB_UIDS']) || $result;
+        return true;
+    }
 
-	/**
-	 * Search for player statistics by scope
-	 *
-	 * @param array $fields
-	 * @param string $scope Scope Array
-	 * @return true
-	 */
-	public static function buildPlayerStatsByScope(&$fields, $scope) {
-		$result = false;
-		$result = self::setField($fields,'PLAYERSTAT.SAISON', OP_IN_INT, $scope['SAISON_UIDS']) || $result;
-		$result = self::setField($fields,'PLAYERSTAT.AGEGROUP', OP_IN_INT, $scope['GROUP_UIDS']) || $result;
-		$result = self::setField($fields,'PLAYERSTAT.COMPETITION', OP_IN_INT, $scope['COMP_UIDS']) || $result;
-		$result = self::setField($fields,'PLAYERSTAT.CLUB', OP_IN_INT, $scope['CLUB_UIDS']) || $result;
-		return true;
-	}
-	/**
-	 * Search for coach statistics by scope
-	 *
-	 * @param array $fields
-	 * @param string $scope Scope Array
-	 * @return true
-	 */
-	public static function buildCoachStatsByScope(&$fields, $scope) {
-		$result = false;
-		$result = self::setField($fields,'COACHSTAT.SAISON', OP_IN_INT, $scope['SAISON_UIDS']) || $result;
-		$result = self::setField($fields,'COACHSTAT.AGEGROUP', OP_IN_INT, $scope['GROUP_UIDS']) || $result;
-		$result = self::setField($fields,'COACHSTAT.COMPETITION', OP_IN_INT, $scope['COMP_UIDS']) || $result;
-		$result = self::setField($fields,'COACHSTAT.CLUB', OP_IN_INT, $scope['CLUB_UIDS']) || $result;
-		return true;
-	}
-	/**
-	 * Search for referee statistics by scope
-	 *
-	 * @param array $fields
-	 * @param string $scope Scope Array
-	 * @return true
-	 */
-	public static function buildRefereeStatsByScope(&$fields, $scope) {
-		$result = false;
-		$result = self::setField($fields,'REFEREESTAT.SAISON', OP_IN_INT, $scope['SAISON_UIDS']) || $result;
-		$result = self::setField($fields,'REFEREESTAT.AGEGROUP', OP_IN_INT, $scope['GROUP_UIDS']) || $result;
-		$result = self::setField($fields,'REFEREESTAT.COMPETITION', OP_IN_INT, $scope['COMP_UIDS']) || $result;
-		$result = self::setField($fields,'REFEREESTAT.CLUB', OP_IN_INT, $scope['CLUB_UIDS']) || $result;
-		return true;
-	}
-	public static function setField(&$fields, $field, $operator, $value) {
-		$result = false;
-		if(strlen(trim($value))) {
-			$fields[$field][$operator] = $value;
-			$result = true;
-		}
-		return $result;
-	}
-}
+    /**
+     * Search for coach statistics by scope
+     *
+     * @param array $fields
+     * @param string $scope
+     *            Scope Array
+     * @return true
+     */
+    public static function buildCoachStatsByScope(&$fields, $scope)
+    {
+        $result = false;
+        $result = self::setField($fields, 'COACHSTAT.SAISON', OP_IN_INT, $scope['SAISON_UIDS']) || $result;
+        $result = self::setField($fields, 'COACHSTAT.AGEGROUP', OP_IN_INT, $scope['GROUP_UIDS']) || $result;
+        $result = self::setField($fields, 'COACHSTAT.COMPETITION', OP_IN_INT, $scope['COMP_UIDS']) || $result;
+        $result = self::setField($fields, 'COACHSTAT.CLUB', OP_IN_INT, $scope['CLUB_UIDS']) || $result;
+        return true;
+    }
 
+    /**
+     * Search for referee statistics by scope
+     *
+     * @param array $fields
+     * @param string $scope
+     *            Scope Array
+     * @return true
+     */
+    public static function buildRefereeStatsByScope(&$fields, $scope)
+    {
+        $result = false;
+        $result = self::setField($fields, 'REFEREESTAT.SAISON', OP_IN_INT, $scope['SAISON_UIDS']) || $result;
+        $result = self::setField($fields, 'REFEREESTAT.AGEGROUP', OP_IN_INT, $scope['GROUP_UIDS']) || $result;
+        $result = self::setField($fields, 'REFEREESTAT.COMPETITION', OP_IN_INT, $scope['COMP_UIDS']) || $result;
+        $result = self::setField($fields, 'REFEREESTAT.CLUB', OP_IN_INT, $scope['CLUB_UIDS']) || $result;
+        return true;
+    }
 
-if (defined('TYPO3_MODE') && $TYPO3_CONF_VARS[TYPO3_MODE]['XCLASS']['ext/t3sportstats/search/class.tx_t3sportstats_search_Builder.php']) {
-	include_once($TYPO3_CONF_VARS[TYPO3_MODE]['XCLASS']['ext/t3sportstats/search/class.tx_t3sportstats_search_Builder.php']);
+    public static function setField(&$fields, $field, $operator, $value)
+    {
+        $result = false;
+        if (strlen(trim($value))) {
+            $fields[$field][$operator] = $value;
+            $result = true;
+        }
+        return $result;
+    }
 }
--- search/class.tx_t3sportstats_search_PlayerStats.php
+++ search/class.tx_t3sportstats_search_PlayerStats.php
@@ -2,7 +2,7 @@
 /***************************************************************
  *  Copyright notice
  *
- *  (c) 2010-2016 Rene Nitzsche
+ *  (c) 2010-2017 Rene Nitzsche
  *  Contact: rene@system25.de
  *  All rights reserved
  *
@@ -20,67 +20,76 @@
  * License along with this library; if not, write to the Free Software
  * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
  ***************************************************************/
-
 tx_rnbase::load('tx_rnbase_util_SearchBase');
-
 
 /**
  * Class to search player stats from database
  *
  * @author Rene Nitzsche
  */
-class tx_t3sportstats_search_PlayerStats extends tx_rnbase_util_SearchBase {
+class tx_t3sportstats_search_PlayerStats extends tx_rnbase_util_SearchBase
+{
 
-	protected function getTableMappings() {
-		$tableMapping['PLAYERSTAT'] = 'tx_t3sportstats_players';
-		$tableMapping['PLAYER'] = 'tx_cfcleague_profiles';
-		$tableMapping['MATCH'] = 'tx_cfcleague_games';
-		$tableMapping['COMPETITION'] = 'tx_cfcleague_competition';
-		$tableMapping['CLUB'] = 'tx_cfcleague_club';
-		$tableMapping['CLUBOPP'] = 'tx_cfcleague_club';
-		// Hook to append other tables
-		tx_rnbase_util_Misc::callHook('t3sportstats','search_PlayerStats_getTableMapping_hook',
-			array('tableMapping' => &$tableMapping), $this);
-		return $tableMapping;
-	}
+    protected function getTableMappings()
+    {
+        $tableMapping = [];
+        $tableMapping['PLAYERSTAT'] = 'tx_t3sportstats_players';
+        $tableMapping['PLAYER'] = 'tx_cfcleague_profiles';
+        $tableMapping['MATCH'] = 'tx_cfcleague_games';
+        $tableMapping['COMPETITION'] = 'tx_cfcleague_competition';
+        $tableMapping['CLUB'] = 'tx_cfcleague_club';
+        $tableMapping['CLUBOPP'] = 'tx_cfcleague_club';
+        // Hook to append other tables
+        tx_rnbase_util_Misc::callHook('t3sportstats', 'search_PlayerStats_getTableMapping_hook', [
+            'tableMapping' => &$tableMapping
+        ], $this);
+        return $tableMapping;
+    }
 
-	protected function useAlias() {return true;}
-	protected function getBaseTableAlias() {
-		return 'PLAYERSTAT';
-	}
-	protected function getBaseTable() {
-		return 'tx_t3sportstats_players';
-	}
-	function getWrapperClass() {
-		return 'tx_t3sportstats_models_PlayerStat';
-	}
+    protected function useAlias()
+    {
+        return true;
+    }
 
-	protected function getJoins($tableAliases) {
-		$join = '';
-		if(isset($tableAliases['MATCH'])) {
-			$join .= ' JOIN tx_cfcleague_games AS MATCH ON PLAYERSTAT.t3match = MATCH.uid ';
-		}
-		if(isset($tableAliases['PLAYER'])) {
-			$join .= ' JOIN tx_cfcleague_profiles AS PLAYER ON PLAYERSTAT.player = PLAYER.uid ';
-		}
-		if(isset($tableAliases['COMPETITION'])) {
-			$join .= ' JOIN tx_cfcleague_competition AS COMPETITION ON COMPETITION.uid = PLAYERSTAT.competition ';
-		}
-		if(isset($tableAliases['CLUB'])) {
-			$join .= ' JOIN tx_cfcleague_club AS CLUB ON CLUB.uid = PLAYERSTAT.club ';
-		}
-		if(isset($tableAliases['CLUBOPP'])) {
-			$join .= ' JOIN tx_cfcleague_club AS CLUBOPP ON CLUBOPP.uid = PLAYERSTAT.clubopp ';
-		}
+    protected function getBaseTableAlias()
+    {
+        return 'PLAYERSTAT';
+    }
 
-		// Hook to append other tables
-		tx_rnbase_util_Misc::callHook('t3sportstats','search_PlayerStats_getJoins_hook',
-			array('join' => &$join, 'tableAliases' => $tableAliases), $this);
-		return $join;
-	}
-}
+    protected function getBaseTable()
+    {
+        return 'tx_t3sportstats_players';
+    }
 
+    function getWrapperClass()
+    {
+        return 'tx_t3sportstats_models_PlayerStat';
+    }
 
-if (defined('TYPO3_MODE') && $TYPO3_CONF_VARS[TYPO3_MODE]['XCLASS']['ext/t3sportstats/search/class.tx_t3sportstats_search_PlayerStats.php']) {
-	include_once($TYPO3_CONF_VARS[TYPO3_MODE]['XCLASS']['ext/t3sportstats/search/class.tx_t3sportstats_search_PlayerStats.php']);
+    protected function getJoins($tableAliases)
+    {
+        $join = '';
+        if (isset($tableAliases['MATCH'])) {
+            $join .= ' JOIN tx_cfcleague_games AS MATCH ON PLAYERSTAT.t3match = MATCH.uid ';
+        }
+        if (isset($tableAliases['PLAYER'])) {
+            $join .= ' JOIN tx_cfcleague_profiles AS PLAYER ON PLAYERSTAT.player = PLAYER.uid ';
+        }
+        if (isset($tableAliases['COMPETITION'])) {
+            $join .= ' JOIN tx_cfcleague_competition AS COMPETITION ON COMPETITION.uid = PLAYERSTAT.competition ';
+        }
+        if (isset($tableAliases['CLUB'])) {
+            $join .= ' JOIN tx_cfcleague_club AS CLUB ON CLUB.uid = PLAYERSTAT.club ';
+        }
+        if (isset($tableAliases['CLUBOPP'])) {
+            $join .= ' JOIN tx_cfcleague_club AS CLUBOPP ON CLUBOPP.uid = PLAYERSTAT.clubopp ';
+        }
+
+        // Hook to append other tables
+        tx_rnbase_util_Misc::callHook('t3sportstats', 'search_PlayerStats_getJoins_hook', [
+            'join' => &$join,
+            'tableAliases' => $tableAliases
+        ], $this);
+        return $join;
+    }
 }
